716b69b6a69a52f747f42e0e2c7430749534dbe2
Saksham Banga <banga.1@iitj.ac.in>
2020-03-24 12:59:33 +0530

    Add files via upload

---
+++ app/src/main/java/com/jpg/coronatracker/App.java
@@ -0,0 +1,31 @@
+package com.example.service_example;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+public class App extends Application {
+    public static final String CHANNEL_ID = "exampleServiceChannel";
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();;
+        createNotificationChannel();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void createNotificationChannel()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(CHANNEL_ID, "Example Service Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+
+    }
+}
--- app/src/main/java/com/jpg/coronatracker/MainActivity.java
+++ app/src/main/java/com/jpg/coronatracker/MainActivity.java
@@ -1,13 +1,88 @@
-package com.jpg.coronatracker;
+//package com.codinginflow.foregroundserviceexample;
+package com.example.service_example;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+//import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextInput = findViewById(R.id.edit_text_input);
+    }
+
+    public void startService(View v) {
+        String input = editTextInput.getText().toString();
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this, "Please grant Permission to access your phone number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        TelephonyManager tMgr = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show();
+        /* Extract the IMEI of the person */
+        input = tMgr.getDeviceId();
+        //Boilerplate code to write a message to the database. Change/Create the hierarchy in getReference and set it's value to something
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(input);
+        myRef.setValue(input);
+
+
+
+
+
+
+//        To use Firebase Storage
+//        FirebaseStorage storage = FirebaseStorage.getInstance();
+//        StorageReference storageRef = storage.getReference();
+
+
+
+
+
+
+
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        serviceIntent.putExtra("inputExtra", input);
+
+        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    public void stopService(View v) {
+        Intent serviceIntent = new Intent(this, ExampleService.class);
+        stopService(serviceIntent);
     }
 }
